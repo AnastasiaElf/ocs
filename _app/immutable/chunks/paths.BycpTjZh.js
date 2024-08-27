80b542ff983b60eaad35239baf3173a3452beb12
AnastasiaElf <anastasia.elf.17@gmail.com>
2024-08-27 17:26:16 +0200

    release v1.2

--- _app/immutable/chunks/paths.BycpTjZh.js
+++
@@ -1,1 +0,0 @@
-var s;const b=((s=globalThis.__sveltekit_kib7bx)==null?void 0:s.base)??"/ocs";var a;const e=((a=globalThis.__sveltekit_kib7bx)==null?void 0:a.assets)??b;export{e as a,b};
---
+++ _app/immutable/entry/start.0jv02sFr.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Db5vq5n_.js";export{t as start};
